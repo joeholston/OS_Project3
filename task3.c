d6c45c309834f80229fe31ccf94662796d2b219e
Joe Holston <holstonjx1@gcc.edu>
2018-05-06 16:14:19 -0400

    Created memCheck function

--- task3.c
+++ task3.c
@@ -3,19 +3,24 @@
 
 #define THRESHOLD 85
 
-int main(int argc, char *argv[]){
-  double percentUsed;
-  struct sysinfo info;
-  sysinfo (&info);
+void memCheck()
+{
+    double percentUsed;
+    struct sysinfo info;
+    sysinfo (&info);
 
-  percentUsed = ((double)(info.totalram - info.freeram) / (double)info.totalram)*100;
-  //printf("%f\n", percentUsed);
-  if(percentUsed >= THRESHOLD){
-    printf("WARNING: System Memory Usage has exceeded %d%%\n", THRESHOLD);
-  }
-  else{
-    printf("You're good!\n");
-  }
+    percentUsed = ((double)(info.totalram - info.freeram) / (double)info.totalram)*100;
+    if(percentUsed >= THRESHOLD){
+      printf("WARNING: System Memory Usage has exceeded %d%%\n", THRESHOLD);
+    }
+    else{
+      printf("You're good!\n");
+    }
+}
+
+int main(int argc, char *argv[]){
+
+  memCheck();
 
   return 0;
 }
